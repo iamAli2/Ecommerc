e107b1d80eae2847f4a7bb66d5a57b5a6f5311fd
Ali Aryan <123121594+iamAli2@users.noreply.github.com>
2023-12-06 01:43:21 +0500

    Aryan- Project Setup

---
+++ lib/Utils/Constant/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+//constant color
+const kDarkBlue = Color(0xFF07052A);
+const kgreyColor = Color(0xFF8C919D);
+const kLightGreyColor = Color(0xFFF1F1FC);
+const klightBlue = Color(0xFF4D65C0);
+const korangeColor = Color(0xFFE95F36);
+const kgreenTextColor = Color(0xFF2CA66F);
+
+//grediants
+const kprimaryColor =
+    LinearGradient(colors: [Color(0xFF6633FF), Color(0xFF20BD52)]);
+const kGrediantButtonColor =
+    LinearGradient(colors: [Color(0xFFE12B42), Color(0xFFFFEA17)]);
